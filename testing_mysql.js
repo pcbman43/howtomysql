9cbf4e976c7eab075b918a8906dcd7c3da9d3ef8
MKougia <kougia.mihkel@khk.ee>
2022-02-07 11:36:37 +0200

    Completed "MySQL Create Table"

--- testing_mysql.js
+++ testing_mysql.js
@@ -3,14 +3,16 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "qwerty"
+  password: "qwerty",
+  database: "mydb"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (err, result) {
+  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Database created");
+    console.log("Table created");
   });
 });
