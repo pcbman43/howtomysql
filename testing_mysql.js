4f42cb0435bc2b6a1bb8b24c18b30fe62e4f93c5
MKougia <kougia.mihkel@khk.ee>
2022-02-07 11:40:06 +0200

    MySQL Insert Into; Example 1

--- testing_mysql.js
+++ testing_mysql.js
@@ -10,9 +10,9 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table created");
+    console.log("1 record inserted");
   });
 });
