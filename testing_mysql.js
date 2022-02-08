dddcd8b6edf76bf62452cec2a6027aa7c08936fc
MKougia <kougia.mihkel@khk.ee>
2022-02-08 09:03:42 +0200

    MySQL Limit; Example 2

--- testing_mysql.js
+++ testing_mysql.js
@@ -9,7 +9,7 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) throw err;
-  var sql = "SELECT * FROM customers LIMIT 5";
+  var sql = "SELECT * FROM customers LIMIT 5 OFFSET 2";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log(result);
