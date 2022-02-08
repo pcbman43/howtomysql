491e39635e3d828409292e2a471cc2748566a764
MKougia <kougia.mihkel@khk.ee>
2022-02-08 08:53:36 +0200

    MySQL Where; Example 4

--- testing_mysql.js
+++ testing_mysql.js
@@ -8,8 +8,8 @@ var con = mysql.createConnection({
 });
 
 var adr = 'Mountain 21';
-var sql = 'SELECT * FROM customers WHERE address = ' + mysql.escape(adr);
-con.query(sql, function (err, result) {
+var sql = 'SELECT * FROM customers WHERE address = ?';
+con.query(sql, [adr], function (err, result) {
   if (err) throw err;
   console.log(result);
 });
